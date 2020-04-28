4c2f023c202a2261cdac5bacb7b6a337f99e37be
Elena Silina <silina.prQgmail.com>
2020-04-28 16:17:11 -0700

    fixed article schema

--- models/article.js
+++ models/article.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose');
 
 const articleSchema = new mongoose.Schema({
-  name: {
+  keyword: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
   },
   link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /^https?:\/\/((((([0-1]?[0-9]?[0-9])|(2[0-4][0-9])|(25[0-5]))\.){3}(([0-1]?[0-9]?[0-9])|(2[0-4][0-9])|(25[0-5])))|((www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}\/?))(:\d{2,5})?(\/([0-9a-zA-Z/._:-]+)?#?)?$/.test(v);
+      },
+    },
+  },
+  image: {
     type: String,
     required: true,
     validate: {
@@ -21,15 +44,12 @@ const articleSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  likes: [{
-    default: [],
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-  }],
-  createdAt: {
-    default: Date.now,
-    type: Date,
-  },
 });
+
+articleSchema.methods.omitPrivate = function omitPrivate() {
+  const obj = this.toObject();
+  delete obj.qwner;
+  return obj;
+};
 
 module.exports = mongoose.model('article', articleSchema);
